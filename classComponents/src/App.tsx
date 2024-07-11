a216213a32f1dae745e08118cc24f182696970cc
whowouldwin <leranetwork@gmail.com>
2024-07-11 18:01:57 +0300

    refactor: convert App component to functional

--- classComponents/src/App.tsx
+++ classComponents/src/App.tsx
@@ -1,18 +1,10 @@
-import { Component } from 'react';
-import { Result } from './interfaces';
+
+
 import './App.css';
 import ErrorBoundary from './components/ErrorBoundary';
 import ChildComponents from './components/ChildComponents';
 
-interface State {
-  results: Result[];
-  loading: boolean;
-  error: boolean;
-  showResults: boolean;
-}
-
-class App extends Component<object, State> {
-  render() {
+const App = () => {
     return (
       <div className="app">
         <ErrorBoundary>
@@ -20,7 +12,6 @@ class App extends Component<object, State> {
         </ErrorBoundary>
       </div>
     );
-  }
 }
 
 export default App;
