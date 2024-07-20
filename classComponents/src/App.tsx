6a863809d22073414126a673f9d4e86627b1d549
whowouldwin <leranetwork@gmail.com>
2024-07-21 01:21:20 +0300

    feat: add custom theme (Context API)

--- classComponents/src/App.tsx
+++ classComponents/src/App.tsx
@@ -4,13 +4,17 @@ import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import SearchComponent from './components/SearchComponent/SearchComponent';
 import NotFound from './components/NotFound/NotFound';
 import DetailComponent from './components/Detail/DetailComponent';
+import { ThemeProvider } from './context/ThemeContext';
 import Flyout from './components/Flyout/Flyout';
+import ThemeToggle from './ThemeToggle/ThemeToggle';
 
 const App = () => {
   return (
+  <ThemeProvider>
     <BrowserRouter>
       <div className="app">
         <ErrorBoundary>
+        <ThemeToggle />
           <Routes>
             <Route path="/" element={<SearchComponent />}>
               <Route path="details/:id" element={<DetailComponent />} />
@@ -21,6 +25,7 @@ const App = () => {
         </ErrorBoundary>
       </div>
     </BrowserRouter>
+  </ThemeProvider>
   );
 };
 
---
+++ classComponents/src/context/ThemeContext.tsx
@@ -0,0 +1,40 @@
+import { createContext, useState, useContext, ReactNode, useEffect } from 'react';
+
+type Theme = 'light' | 'dark';
+
+interface ThemeContextType {
+  theme: Theme;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [theme, setTheme] = useState<Theme>(() => {
+    const savedTheme = localStorage.getItem('theme');
+    return (savedTheme as Theme) || 'light';
+  });
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+    document.body.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = (): ThemeContextType => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useTheme must be used within ThemeProvider');
+  }
+  return context;
+};
